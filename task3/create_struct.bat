a57fb7cf4b196ae58a9dbbba6ef94d597493d6c9
Makarov Roman <43114256+Lemyr621@users.noreply.github.com>
2018-09-30 15:44:21 +0300

    создание папки происходит по месту вызова батника

--- task3/create_struct.bat
+++ task3/create_struct.bat
@@ -1,45 +1,61 @@
 set /p x="BBeduTe umia papki: "
 @Echo Off
 
+Set string1=html{
+Set "string2="
+Set string3=}
+Set "string 4="
+Set string5=body {
+Set "string6="
+Set srting7=}
+Set "string8="
+Set string9=.content {
+Set "string10="
+Set string11=}
+Set "string12="
+Set string13=.content h1 {
+Set "string14="
+Set string15=}
 Set Papka=D:\%x%
+Set string=html{
 
 If not Exist "%Papka%\*.*" (
 MD %x%
-MD D:\%x%\css
-MD D:\%x%\js
+MD %x%\css
+MD %x%\js
 echo Create style.css
-echo. html {>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. }>>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. body {>>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. }>>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. .content {>>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. }>>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. .content h1 {>>D:\%x%\css\style.css
-echo. >>D:\%x%\css\style.css
-echo. }>>D:\%x%\css\style.css
+echo. %string1%>%x%\css\style.css
+echo. %string2%>>%x%\css\style.css
+echo. %string3%>>%x%\css\style.css
+echo. %string4%>>%x%\css\style.css
+echo. %string5%>>%x%\css\style.css
+echo. %string6%>>%x%\css\style.css
+echo. %string7%>>%x%\css\style.css
+echo. %string8%>>%x%\css\style.css
+echo. %string9%>>%x%\css\style.css
+echo. %string10%>>%x%\css\style.css
+echo. %string11%>>%x%\css\style.css
+echo. %string12%>>%x%\css\style.css
+echo. %string13%>>%x%\css\style.css
+echo. %string14%>>%x%\css\style.css
+echo. %string15%>>%x%\css\style.css
 echo Create app.js
-echo. >D:\%x%\js\app.js
+echo. >%x%\js\app.js
 echo Create index.html
-echo. ^<^!DOCTYPE html^>^ >D:\%x%\index.html
-echo. ^<^html lang="en"^>^ >>D:\%x%\index.html
-echo. ^<^head^>^ >>D:\%x%\index.html
-echo.   ^<^meta charset="UTF-8"^>^ >>D:\%x%\index.html
-echo.   ^<^title>Title</title^>^ >>D:\%x%\index.html
-echo.   ^<^link rel="stylesheet" href="css/style.css"/^>^ >>D:\%x%\index.html
-echo. ^<^/head^>^ >>D:\%x%\index.html
-echo. ^<^body^>^ >>D:\%x%\index.html
-echo.   ^<^div class="content"^>^ >>D:\%x%\index.html
-echo.     ^<^h1^>^Hello! I'm ready for edit! :)^<^/h1^>^ >>D:\%x%\index.html
-echo.   ^<^/div^>^ >>D:\%x%\index.html
-echo.   ^<^script src="js/app.js"^>^^<^/script^>^ >>D:\%x%\index.html
-echo. ^<^/body^>^ >>D:\%x%\index.html
-echo. ^<^/html^>^ >>D:\%x%\index.html
+echo. ^<^!DOCTYPE html^>^ >%x%\index.html
+echo. ^<^html lang="en"^>^ >>%x%\index.html
+echo. ^<^head^>^ >>%x%\index.html
+echo.   ^<^meta charset="UTF-8"^>^ >>%x%\index.html
+echo.   ^<^title>Title</title^>^ >>%x%\index.html
+echo.   ^<^link rel="stylesheet" href="css/style.css"/^>^ >>%x%\index.html
+echo. ^<^/head^>^ >>%x%\index.html
+echo. ^<^body^>^ >>%x%\index.html
+echo.   ^<^div class="content"^>^ >>%x%\index.html
+echo.     ^<^h1^>^Hello! I'm ready for edit! :)^<^/h1^>^ >>%x%\index.html
+echo.   ^<^/div^>^ >>%x%\index.html
+echo.   ^<^script src="js/app.js"^>^^<^/script^>^ >>%x%\index.html
+echo. ^<^/body^>^ >>%x%\index.html
+echo. ^<^/html^>^ >>%x%\index.html
 Echo The project created!
 ) Else (
 Echo The project is already created!
