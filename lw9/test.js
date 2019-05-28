c1cbc2248948c4bd937a7d8be7cb637194553bb4
Makarov Roman <43114256+Lemyr621@users.noreply.github.com>
2019-05-29 00:37:56 +0300

    uf

--- lw9/test.js
+++ lw9/test.js
@@ -1,51 +1,22 @@
-describe("Проверка номера", function() {
+describe("Проверка номера/адреса", function() {
 
-  function makeTest(x, y) {
-    it("при указании номера " + x + " и отправке кода " + y, function() {
-      assert.equal(SMS(x, y), true);
+  function makeTest(x) {
+    it("при указании номера/адреса" + x , function() {
+      assert.equal(Send(x), "111");
     });
   }
 
-  function test(checkNomber, userCode) {
-    makeTest(checkNomber, userCode);
-    var check = "111111"
-    makeTest(check, userCode);
-    var check = "11111132143241111"
-    makeTest(check, userCode);
-    var check = "111фв11111"
-    makeTest(check, userCode);
-    var check = ""
-    makeTest(check, userCode);
-  }
-  var checkNomber = "1111111111"
-  var userCode = "1111"
-  test(checkNomber, userCode);
-  var userCode = "2222"
-  test(checkNomber, userCode);
+    var check = "1234567891"
+    makeTest(check);
 });
 
-describe("Проверка почты", function() {
+describe("Проверка кода", function() {
 
-  function makeTest(x, y) {
-    it("при указании почты " + x + " и отправке кода " + y, function() {
-      assert.equal(Email(x, y), true);
+  function makeTest(x) {
+    it("при указании кода " + x, function() {
+      assert.equal(check(x), true);
     });
   }
-
-  function test(check, userCode) {
-    makeTest(check, userCode);
-    var check = "@mail.mail"
-    makeTest(check, userCode);
-    var check = "mail@mail"
-    makeTest(check, userCode);
-    var check = "mail@mail."
-    makeTest(check, userCode);
-    var check = ""
-    makeTest(check, userCode);
-  }
-  var check = "mail@mail.mail"
-  var userCode = "1111"
-  test(check, userCode)
-  var userCode = "2222"
-  test(check, userCode)
+    var code = "111"
+    makeTest(code);
 });
