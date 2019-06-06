667a184718ecf381bb692e6cc042b86f0b0db000
Makarov Roman <43114256+Lemyr621@users.noreply.github.com>
2019-06-06 13:16:51 +0300

    moc

--- lw3/task3/scripts/script.js
+++ lw3/task3/scripts/script.js
@@ -1,12 +1,12 @@
-hall='';
-for(i=1;i<=10;i++){
-  row="";
-  for(j=1;j<=10;j++){
-    row=row+'<div class="seat"><p>'+j+'</p></div>';
+hall = '';
+for (i = 1; i <= 10; i++) {
+  row = "";
+  for (j = 1; j <= 10; j++) {
+    row = row + '<div class="seat"><p>' + j + '</p></div>';
   }
-  hall=hall+row+'<div class="SpaceBetween"></div>';
+  hall = hall + row + '<div class="SpaceBetween"></div>';
 }
 $('div.Hall').html(hall);
-$('.seat').click(function(){
-$(event.currentTarget).toggleClass('bay');
+$('.seat').click(function() {
+  $(event.currentTarget).toggleClass('bay');
 });
