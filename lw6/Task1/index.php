621e6c2126ab73d7acb465c1be12ecb291dcec77
Makarov Roman <43114256+Lemyr621@users.noreply.github.com>
2019-06-19 19:10:24 +0300

    j

--- lw6/Task1/index.php
+++ lw6/Task1/index.php
@@ -5,20 +5,48 @@
 		<title>VK API</title>
 
 <meta charset="utf-8">
-<link href="style.css" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <?php // ключ приложения
-$url = file_get_contents("https://api.vk.com/method/wall.get?owner_id=-26750264&count=15&filter=owner&v=5.92&access_token=b0e05daab0e05daab0e05daa0cb08b7414bb0e0b0e05daaede606ea22d8738d28a04fe4"); // функция по которой подключаемся к нужной странице и используем нужный метод
+$url = file_get_contents("https://api.vk.com/method/wall.get?owner_id=-26750264&count=100&filter=owner&v=5.92&access_token=b0e05daab0e05daab0e05daa0cb08b7414bb0e0b0e05daaede606ea22d8738d28a04fe4"); // функция по которой подключаемся к нужной странице и используем нужный метод
 $wall = json_decode($url);
 $wall = $wall->response->items;
-for ($i = 0; $i < count($wall); $i++) {
-	if(strpos($wall[$i]->text,'Страна:')){
-	echo "<p>".($i + 1)."</b>. <i>".$wall[$i]->text."</p><br />";
-	echo"<p><Img src=\"".$wall[$i]->attachments[0]->photo->sizes[0]->url."\"</p>";
-	echo "<p>Лайков ".$wall[$i]->likes->count."</p>";}
-
+$postNumber = 1;
+$PostIdArray = array();
+for ($i = 0;$i < 15;$i++) {
+    $randPostId = rand(0, 100);
+    if (!in_array($randPostId, $PostIdArray)) {
+        $PostIdArray[] = $randPostId;
+        if (strpos($wall[$randPostId]->text, 'Страна:')) {
+            $postText = $wall[$randPostId]->text;
+            $countryPos = strpos($postText, 'Страна:');
+            $name = substr($postText, 0, $countryPos);
+            $postText = substr($postText, $countryPos, strlen($postText) - 1);
+            $genrePos = strpos($postText, 'Жанр:');
+            $country = substr($postText, 0, $genrePos);
+            $postText = substr($postText, $genrePos, strlen($postText) - 1);
+            $ratingPos = strpos($postText, 'Рейтинги:');
+            $genre = substr($postText, 0, $ratingPos);
+            $genre = str_replace("@xfilm", "", $genre);
+            $postText = substr($postText, $ratingPos, strlen($postText) - 1);
+            $producerPos = strpos($postText, 'Режиссер:');
+            $rating = substr($postText, 0, $producerPos);
+            $postText = substr($postText, $producerPos, strlen($postText) - 1);
+            $actorPos = strpos($postText, 'Режиссер:');
+            $producer = substr($postText, 0, $actorPos);
+            $postText = substr($postText, $actorPos, strlen($postText) - 1);
+            echo "<p>" . ($postNumber) . "</b>. <i>" . $name . "</p><br />";
+            echo "<p><Img src=\"" . $wall[$randPostId]->attachments[0]->photo->sizes[2]->url . "\"</p>";
+            echo "<p>" . $country . "</p>";
+            echo "<p>" . $genre . "</p>";
+            echo "<p>" . $rating . "</p>";
+            echo "<p>" . $producer . "</p>";
+            echo "<p>" . $postText . "</p>";
+            echo "<p>Лайков " . $wall[$randPostId]->likes->count . "</p>";
+            echo "<a href='https://vk.com/xfilm?w=wall-26750264_" . $wall[$randPostId]->id . "'> Смотреть на стене</a>";
+            $postNumber++;
+        }
+    }
 }
 ?>
 </html>
