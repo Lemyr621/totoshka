8c1eb29f3b3de90dcfe2c9e86d213832a5ca7954
Makarov Roman <43114256+Lemyr621@users.noreply.github.com>
2019-06-25 20:41:32 +0300

    lw5

--- lw5/scripts/script.js
+++ lw5/scripts/script.js
@@ -18,6 +18,29 @@ mail_field.onfocus = function() {
   mail_field.parentNode.removeChild(mail_alert_id);
 }
 
+
+function checkNumber(str) {
+  if (str.length !== 11) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+number_field.onblur = function() {
+  if (!checkNumber(this.value)) {
+    numberAddErrMesage();
+  } else {
+    document.getElementsByClassName('number_field')[0].style.backgroundColor = 'green';
+  }
+}
+number_field.onfocus = function() {
+  document.getElementsByClassName('number_field')[0].style.backgroundColor = '#484d56';
+  number_field.parentNode.removeChild(number_alert_id);
+}
+
+
+
 password_field.onblur = function() {
   var pass = document.getElementsByClassName('password-block__password')[0].value;
   if (!checkPassword(pass, this.value)) {
@@ -78,6 +101,18 @@ function mailAddErrMesage() {
     div.innerHTML = "you have entered an incorrect login";
     mail_field.parentNode.insertBefore(div, mail_field.nextSibling);
     document.getElementsByClassName('registration__login')[0].style.backgroundColor = 'red';
+  }
+}
+
+function numberAddErrMesage() {
+  var bloop = document.getElementById('number_alert_id')
+  if (bloop === null) {
+    var div = document.createElement('div');
+    div.className = "alert";
+    div.id = "number_alert_id"
+    div.innerHTML = "you have entered an incorrect number";
+    number_field.parentNode.insertBefore(div, number_field.nextSibling);
+    document.getElementsByClassName('number_field')[0].style.backgroundColor = 'red';
   }
 }
 
