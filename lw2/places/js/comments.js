f98744890d081b722fecac723919488b632ac0d2
Makarov Roman <43114256+Lemyr621@users.noreply.github.com>
2019-06-25 16:08:23 +0300

    c

--- lw2/places/js/comments.js
+++ lw2/places/js/comments.js
@@ -3,13 +3,22 @@ function addComment() {
 
   var userNick = document.createElement('div');
   userNick.className = "nick";
+  userNick.id = 'user_nick';
   var nickText = document.getElementById('nick');
   userNick.innerHTML = nickText.value;
 
   var newComment = document.createElement('div');
   newComment.className = "comment";
+  userNick.id = 'user_comment';
   var commentText = document.getElementById('comment');
   newComment.innerHTML = commentText.value;
   commentList.appendChild(userNick);
   commentList.appendChild(newComment);
+
+  var elemCount  = document.getElementsByClassName("comments")[0].childElementCount;
+  console.log(elemCount);
+  if(elemCount > 8){
+    commentList.removeChild(commentList.firstChild);
+    commentList.removeChild(commentList.firstChild);
+  }
 }
